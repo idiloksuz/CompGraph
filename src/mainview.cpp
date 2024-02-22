814b4f12c350b47d6548315fd8cd57cd23d640b6
idiloksuz <i.oksuz@student.rug.nl>
2024-02-22 15:42:36 +0100

    first

---
+++ src/mainview.cpp
@@ -0,0 +1,135 @@
+#include "mainview.h"
+
+#include <QDateTime>
+
+/**
+ * @brief MainView::MainView Constructs a new main view.
+ *
+ * @param parent Parent widget.
+ */
+MainView::MainView(QWidget *parent) : QOpenGLWidget(parent) {
+  qDebug() << "MainView constructor";
+
+  connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
+}
+
+/**
+ * @brief MainView::~MainView
+ *
+ * Destructor of MainView
+ * This is the last function called, before exit of the program
+ * Use this to clean up your variables, buffers etc.
+ *
+ */
+MainView::~MainView() {
+  qDebug() << "MainView destructor";
+
+  makeCurrent();
+}
+
+// --- OpenGL initialization
+
+/**
+ * @brief MainView::initializeGL Called upon OpenGL initialization
+ * Attaches a debugger and calls other init functions.
+ */
+void MainView::initializeGL() {
+  qDebug() << ":: Initializing OpenGL";
+  initializeOpenGLFunctions();
+
+  connect(&debugLogger, SIGNAL(messageLogged(QOpenGLDebugMessage)), this,
+          SLOT(onMessageLogged(QOpenGLDebugMessage)), Qt::DirectConnection);
+
+  if (debugLogger.initialize()) {
+    qDebug() << ":: Logging initialized";
+    debugLogger.startLogging(QOpenGLDebugLogger::SynchronousLogging);
+  }
+
+  QString glVersion{reinterpret_cast<const char *>(glGetString(GL_VERSION))};
+  qDebug() << ":: Using OpenGL" << qPrintable(glVersion);
+
+  // Enable depth buffer
+  glEnable(GL_DEPTH_TEST);
+
+  // Enable backface culling
+  glEnable(GL_CULL_FACE);
+
+  // Default is GL_LESS
+  glDepthFunc(GL_LEQUAL);
+
+  // Set the color to be used by glClear. This is, effectively, the background
+  // color.
+  glClearColor(0.37f, 0.42f, 0.45f, 0.0f);
+
+  createShaderProgram();
+}
+
+/**
+ * @brief MainView::createShaderProgram Creates a new shader program with a
+ * vertex and fragment shader.
+ */
+void MainView::createShaderProgram() {
+  shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex,
+                                        ":/shaders/vertshader.glsl");
+  shaderProgram.addShaderFromSourceFile(QOpenGLShader::Fragment,
+                                        ":/shaders/fragshader.glsl");
+  shaderProgram.link();
+}
+
+/**
+ * @brief MainView::paintGL Actual function used for drawing to the screen.
+ *
+ */
+void MainView::paintGL() {
+  // Clear the screen before rendering
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  shaderProgram.bind();
+
+  // Draw here
+
+  shaderProgram.release();
+}
+
+/**
+ * @brief MainView::resizeGL Called upon resizing of the screen.
+ *
+ * @param newWidth The new width of the screen in pixels.
+ * @param newHeight The new height of the screen in pixels.
+ */
+void MainView::resizeGL(int newWidth, int newHeight) {
+  // TODO: Update projection to fit the new aspect ratio
+  Q_UNUSED(newWidth)
+  Q_UNUSED(newHeight)
+}
+
+/**
+ * @brief MainView::setRotation Changes the rotation of the displayed objects.
+ * @param rotateX Number of degrees to rotate around the x axis.
+ * @param rotateY Number of degrees to rotate around the y axis.
+ * @param rotateZ Number of degrees to rotate around the z axis.
+ */
+void MainView::setRotation(int rotateX, int rotateY, int rotateZ) {
+  qDebug() << "Rotation changed to (" << rotateX << "," << rotateY << ","
+           << rotateZ << ")";
+  Q_UNIMPLEMENTED();
+}
+
+/**
+ * @brief MainView::setScale Changes the scale of the displayed objects.
+ * @param scale The new scale factor. A scale factor of 1.0 should scale the
+ * mesh to its original size.
+ */
+void MainView::setScale(float scale) {
+  qDebug() << "Scale changed to " << scale;
+  Q_UNIMPLEMENTED();
+}
+
+/**
+ * @brief MainView::onMessageLogged OpenGL logging function, do not change.
+ *
+ * @param Message The message to be logged.
+ */
+void MainView::onMessageLogged(QOpenGLDebugMessage Message) {
+  qDebug() << " → Log:" << Message;
+}
